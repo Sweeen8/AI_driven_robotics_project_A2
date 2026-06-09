938c84e08c9dad48a27a81f0b686adc969628037
Amyr Lourensz <agjl.lourensz@student.avans.nl>
2026-06-09 14:16:20 +0200

    Amyr Lourensz 2:14 PM / 14:14 2026-06-09
    Heb Lite 6 werkcell enviroment en Rviz configuratie
    
    [Toegevoegd: volledige digitale werkomgeving voor de UFACTORY Lite 6, inclusief tafel, werkplatform, montageplaat, stoel, RViz-configuraties, launchbestand en bijbehorende pakketinstellingen.]

--- src/a2_workcell_description/launch/display_workcell.launch.py
+++ src/a2_workcell_description/launch/display_workcell.launch.py
@@ -1,17 +1,18 @@
 from pathlib import Path
 
+import xacro
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-import xacro
 
 
-def generate_launch_description():
+def generate_launch_description() -> LaunchDescription:
     package_share = Path(
         get_package_share_directory("a2_workcell_description")
     )
 
     xacro_file = package_share / "urdf" / "workcell.urdf.xacro"
+    rviz_config = package_share / "rviz" / "workcell.rviz"
 
     robot_description = xacro.process_file(
         str(xacro_file)
@@ -30,12 +31,17 @@ def generate_launch_description():
                         "use_sim_time": False,
                     }
                 ],
+                remappings=[
+                    ("robot_description", "/workcell_description"),
+                ],
             ),
+
             Node(
                 package="rviz2",
                 executable="rviz2",
-                name="rviz2",
+                name="workcell_rviz",
                 output="screen",
+                #arguments=["-d",str(rviz_config)],
             ),
         ]
     )
